ab2dc0764817ffa029e62bc769071204537fa1af
spoilaro <johannes.haapanen@gmail.com>
2021-03-09 11:56:14 +0200

    Links are now working in Cv section.

--- src/components/CV/Cv.js
+++ src/components/CV/Cv.js
@@ -1,13 +1,21 @@
-import React, { useRef, forwardRef } from 'react';
+import React, { forwardRef } from 'react';
+import LinkButton from '../buttons/LinkButton'
 import cvsc from '../../assets/cvsc.png'
 import "./cv.scss"
 
 const Cv = forwardRef((props, ref) => {
 
+    const cvLink = "https://drive.google.com/file/d/1YaXKpL_98Nu7iShgW56UcPRnC4SjFy47/view?usp=sharing"
+
+    const loadCv = () => {
+        window.open(cvLink);
+    }
+
     return (
         <div>
             <div ref={ref} className="cv-container">
-                <img alt="Image of Cv" id="cvimg" src={cvsc} />
+                <img alt="Cv" id="cvimg" src={cvsc} />
+                <LinkButton onClick={loadCv} link={"View CV in Google Drive"} />
             </div>
         </div>
     );
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,7 +6,15 @@ import linkedinlogo from '../assets/li-icon2.png'
 
 export default function LandingPage(props) {
 
-    
+    const gitUrl = "https://github.com/spoilaro";
+    const liUrl = "https://www.linkedin.com/in/johannes-haapanen-ab0ba4205/"
+
+    const goToGitHub = () => {
+        window.open(gitUrl);
+    }
+    const goToLinkedIn = () => {
+        window.open(liUrl)
+    }
 
     return (
         <div className="landing-container">
@@ -17,8 +25,8 @@ export default function LandingPage(props) {
                 <LinkButton link={"Projects"} />
             </div>
             <div className="media-links" >
-                <img src={gitlogo} />
-                <img src={linkedinlogo} />
+                <img onClick={goToGitHub} alt="git-logo" src={gitlogo} />
+                <img onClick={goToLinkedIn} alt="li-logo" src={linkedinlogo} />
             </div>
         </div>
     )
