7fa237db50714824912b5bfd944c875447f56466
spoilaro <johannes.haapanen@gmail.com>
2021-03-09 14:54:28 +0200

    Backend software is now done and ready for testing.

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const app = express();
+app.use(cors);
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => {console.log("Server is running!");})
+
+const contactEmail = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+        user: process.env.EUSER,
+        pass: process.env.PASS,
+
+    },
+})
+
+contactEmail.verify((error)=> {
+    if (error){
+        console.log(error);
+    } else {
+        console.log("Ready to Send");
+    }
+})
+
+router.post("/contact", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
+    const mail = {
+        from: name,
+        to: process.env.EUSER,
+        subject: "Contact Form Message",
+        html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+    };
+
+    contactEmail.sendMail(mail, (e) => {
+        if (e) {
+            console.log("sending failed");
+        } else {
+            console.log("sending done");
+        }
+    })
+})
