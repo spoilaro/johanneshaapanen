69c058ec3f4c16a5961ba8855d39fc5a1d7963f9
spoilaro <johannes.haapanen@gmail.com>
2021-03-09 14:00:51 +0200

    added contact folder

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <div className="contact-container" >
+                
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
