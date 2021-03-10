eb16bd6576e731f76d77d9070f8f522509a69db7
spoilaro <johannes.haapanen@gmail.com>
2021-03-10 17:30:00 +0200

    Making basic contact page without backend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import LandingPage from "./components/LandingPage"
 import Cv from "./components/CV/Cv"
+import Contact from "./components/contact/Contact"
 import { useRef } from "react"
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
     <div className="App">
         <LandingPage onCvClick={cvScroll} />
         <Cv ref={cvRef} />
+        <Contact/>
     </div>
   );
 }
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import "./contact.scss"
 
 const Contact = () => {
     return (
-        <div>
+        <div className="contact-page" >
             <div className="contact-container" >
-                
+                <h1>Contact me</h1>
+                <h2>You can contact me via email which is below</h2>
+                <h2>johannes.haapanen@gmail.com</h2>
             </div>
         </div>
     );
