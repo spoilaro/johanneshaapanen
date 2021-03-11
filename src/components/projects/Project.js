60e27e614af007267016eff6a8ba9a0e6814dc69
spoilaro <johannes.haapanen@gmail.com>
2021-03-11 08:55:14 +0200

    Projects section almost ready for adding more projects.

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -2,19 +2,29 @@ import React from 'react';
 import LinkButton from "../buttons/LinkButton"
 import "./project.scss"
 
-const Project = () => {
+const Project = (props) => {
+
+    const openGit = () => {
+        window.open(props.gitLink)
+    }
+    const openLive = () => {
+        window.open(props.liveLink)
+    }
+
     return (
         <div>
             <div className="project-container" >
-                <h1>PlaceHolder</h1>
-                <img/>
+                <h1>{props.title}</h1>
+                <div className="image-container" >
+                    <img src={props.image} />
+                </div>
                 <div className="info-container" >
                     <p>
                         dasfkjhsjdakhfjkhsdajkhfkjhasdkjhfhsdkjh kfsd kjfdhs kjfhskjadh fkjsh dkfhskdhf sdhaf ldaskjhfk jhsadkf hskjdh fkjshdakj fhkjahd fhdakshfkj hsdkj 
                     </p>
                     <div className="button-container" >
-                        <LinkButton/>
-                        <LinkButton/>
+                        <LinkButton onClick={openGit} link={"On GitHub"} />
+                        <LinkButton onClick={openLive} link={"See Live"} />
                     </div>
                 </div>
             </div>
--- src/components/projects/ProjectSection.js
+++ src/components/projects/ProjectSection.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Project from "./Project"
 import "./projectsection.scss"
+import livingloanImg from "../../assets/livingloan_preview1.png"
+
+const livingLoanData = {
+    image: livingloanImg,
+    title: "LivingLoan",
+    gitLink: "https://github.com/spoilaro/livingloan",
+    liveLink: "http://livingloan.herokuapp.com/"
+}
+
 
 const ProjectSection = () => {
     return (
         <div>
             <div className="projects-container" >
-                <Project/>
+                <Project {...livingLoanData} />
                 <Project/>
                 <Project/>
             </div>
